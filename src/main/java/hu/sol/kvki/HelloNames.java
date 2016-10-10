c59488b64c00cce78cc61e3032504371792e9d91
tamas.pinter <tamas.pinter@suitsolutions.eu>
2016-10-10 19:21:07 +0200

    hello Names

---
+++ src/main/java/hu/sol/kvki/HelloNames.java
@@ -0,0 +1,30 @@
+package hu.sol.kvki;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet(urlPatterns="/helloNames")
+public class HelloNames extends HttpServlet {
+
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp)
+			throws ServletException, IOException {
+		List<String> names = new ArrayList<String>();
+		names.add("József");
+		names.add("Sándor");
+		names.add("Benedek");
+		req.setAttribute("names", names);
+		RequestDispatcher requestDispatcher = req.getServletContext()
+				.getRequestDispatcher("/hello_names.jsp");
+		requestDispatcher.include(req, resp);
+	}
+
+}
